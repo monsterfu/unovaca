4178895e74ce2913c562a3a298b0c3955963eb16
monsterfu <seanh@seanHtekiMac-mini.local>
2014-08-18 14:30:10 +0800

    vision display

--- nRF Temp/MainViewController.m
+++ nRF Temp/MainViewController.m
@@ -121,7 +121,7 @@
     _textLabel.text = NSLocalizedString(@"没有检测到体温",nil);
     
     UILabel* visionLabel = [[UILabel alloc]initWithFrame:CGRectMake(0, 70, 100, 30)];
-    visionLabel.text = @"1.29";
+    visionLabel.text = @"1.30";
     [self.view addSubview:visionLabel];
     
 }
